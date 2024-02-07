aff9a36efd5aa727cb3350dd897d9efe0561f645
mauricio-hub <mauriciogonzalezdeveloper@gmail.com>
2024-02-07 11:24:15 -0500

    removiendo codigo comentado

--- src/helper/download-image-as-png.ts
+++ src/helper/download-image-as-png.ts
@@ -1,31 +1,3 @@
-/* import * as fs from 'fs';
-import * as path from 'path';
-import * as sharp from 'sharp';
-import { InternalServerErrorException } from '@nestjs/common';
-
-export const downloadImageAsPng = async (url: string) => {
-  const response = await fetch(url);
-
-  if (!response.ok) {
-    throw new InternalServerErrorException('Network response was not ok');
-  }
-
-  const folderPath = path.resolve('./', './generated/images');
-  fs.mkdirSync(folderPath, { recursive: true });
-
-  const imageNamePng = `${new Date().getTime()}.png`;
-
-  const buffer = Buffer.from(await response.arrayBuffer());
-
-  // fs.writeFileSync(`${folderPath}/${imageNamePng}`, buffer);
-  const completePath = path.join(folderPath, imageNamePng);
-
-  await sharp(buffer).png().ensureAlpha().toFile(completePath);
-
-  return path.join(folderPath, imageNamePng);
-};
- */
-
 import * as fs from 'fs';
 import * as path from 'path';
 import fetch from 'node-fetch';
